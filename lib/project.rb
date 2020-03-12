7f66590d220ce4cb42370fdf6b9d14a9cc694134
Gavin Sharkey <gavinsharkey2016@gmail.com>
2020-03-12 20:14:23 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -7,6 +7,7 @@ class Project
   end
 
   def add_backer(backer)
-
+    @backers << backer if backer.is_a?(Backer)
+    backer.back_project(self) if !(backer.backed_projects.include?(self))
   end
 end
